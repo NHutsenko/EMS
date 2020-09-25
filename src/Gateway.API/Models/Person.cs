d352dd33e007535ea31a065dd49e40bf8ca738ab
Гуценко Микита Васильович <m.gutsenko@bankvostok.com.ua>
2020-09-25 14:57:27 +0300

    added staff and person entities

---
+++ src/Gateway.API/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace EMS.Gateway.API.Models
+{
+    public class Person: BaseModel
+    {
+        [IgnoreDataMember]
+        public ICollection<Staff> Staff { get; set; }
+    }
+}
---
+++ src/Gateway.API/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Gateway.API.Models
+{
+    public class Staff: BaseModel
+    {
+        [Column("personId")]
+        public long? PersonId { get; set; }
+        [Column("person")]
+        public Person Person { get; set; }
+        [Column("personId")]
+        public long ManagerId { get; set; }
+        [Column("manager")]
+        public Person Manager { get; set; }
+    }
+}
