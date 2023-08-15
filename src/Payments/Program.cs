8f38428b07f41e620f01c7b8b2e07cae5b0e3910
Mykyta Hutsenko <m.gutsenko@bankvostok.com.ua>
2023-08-15 16:02:03 +0300

    base structure

---
+++ src/Payments/Program.cs
@@ -0,0 +1,23 @@
+using Logging.Extensions;
+using Payments.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc(cfg =>
+{
+    cfg.EnableDetailedErrors = true;
+    cfg.AddServiceLogging();
+    cfg.MaxReceiveMessageSize = null;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ src/Vacations/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using Vacations;
+
+namespace Vacations.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
