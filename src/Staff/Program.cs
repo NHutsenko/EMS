de9a0fd71e80b8420118badf9286268aa51adb74
Mykyta Hutsenko <m.gutsenko@bankvostok.com.ua>
2023-08-25 17:51:12 +0300

    position.proto

--- src/Staff/Program.cs
+++ src/Staff/Program.cs
@@ -1,5 +1,4 @@
 using EMS.Logging.Extensions;
-using EMS.Staff.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +16,6 @@ builder.Services.AddGrpc(cfg =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
